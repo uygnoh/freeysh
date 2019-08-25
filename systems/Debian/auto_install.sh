5497fcf8f6168066834c4e4e71295d995395b6ff
hongyu <961832097@qq.com>
2019-08-25 23:42:22 +0800

    Sun 25 Aug 2019 11:42:22 PM CST

--- systems/Debian/auto_install.sh
+++ systems/Debian/auto_install.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-###############################################################################
 echo -e "\033[41;37m--->>>BEGING INSTALL UTILITIES\033[37;0m"
-### #00FF00,绿色-光标
-### cp /home/ysh/Documents/FREEYSH/profile/molokai.vim /usr/share/vim/vim80/colors
 
-### 适用于Debian 命令#: dpkg-reconfigure tzdata ;用于修改系统时区
 
-### =======================================================
 sudo apt-get -y install gcc
 sudo apt-get -y install gdb
 sudo apt-get -y install make
@@ -29,25 +24,11 @@ sudo apt-get -y install dia
 sudo apt-get -y install cmus 
 sudo apt-get -y install smplayer
 sudo apt-get -y install curl
-
-
 sudo apt-get -y install cryptsetup
-
-### =======================================================
-A=/home/ysh/Documents/FREEYSH/profile/
-B=/home/ysh/
-cp $A.tmux.conf $A.vimrc $B
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
-
-
-
-
-
 
 
 
 echo -e "\033[41;37mINSTALLATION IS COMPLETE--->>>\033[37;0m"
 echo "return $?"
-###############################################################################
+
 
