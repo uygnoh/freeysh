7b19114faafa62756f0861cb7cf078fdcc008f8a
hongyu <961832097@qq.com>
2020-10-24 06:35:58 -0400

    Sat 24 Oct 2020 06:35:58 AM EDT

--- utilities/tmux/tmux_launch.sh
+++ utilities/tmux/tmux_launch.sh
@@ -1,13 +1,12 @@
+#!/bin/bash
+
 SESSION=STM32
-
 tmux new-session -d -s $SESSION
-
 tmux new-window -t $SESSION:1 -n 'user'
 tmux new-window -t $SESSION:2 -n 'libs'
-
 tmux select-window -t $SESSION:1
 tmux split-window -h
-
 tmux send-keys 'cd user' C-m
-
 tmux attach -t $SESSION
+
+
