607f177840afaa91aa1d15d73564c1bcefd7a60c
hongyu <961832097@qq.com>
2020-10-24 04:12:17 -0400

    Sat 24 Oct 2020 04:12:17 AM EDT

--- embedded/projects/cortexm/bsp_stm32/std_lib/usart/usart1.c
+++ embedded/projects/cortexm/bsp_stm32/std_lib/usart/usart1.c
@@ -1,7 +1,12 @@
 
 #include "usart1.h"
-
-void usart1_configure(void)
+/**
+  ****************************************************************************
+  * 串口1(USART1)初始化配置
+  * 2018.10.29
+  * @freeysh
+  */
+void usart_configure(void)
 {
 	/* configure the USART1 */
 	USART_InitTypeDef USART_InitStructure;
@@ -9,10 +14,24 @@ void usart1_configure(void)
 	USART_InitStructure.USART_WordLength = USART_WordLength_8b;
 	USART_InitStructure.USART_StopBits = USART_StopBits_1;
 	USART_InitStructure.USART_Parity = USART_Parity_No;
-	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+	USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_Non;
 	USART_InitStructure.USART_Mode = USART_Mode_Tx | USART_Mode_Rx;
 	USART_Init(USART1, &USART_InitStructure);
-
 	/* Enable the USART1 */
 	USART_Cmd(USART1, ENABLE);
 }
+
+int main(void)
+{
+	int i, data, Status;
+	data = 'A';
+	USART_SendData(USART1, data);
+	
+	for (i = 0; i < 30; i++) {
+		data++;
+		while (USART1_GetFlagStatus(USART1, USART_FLAG_TC) == RESET);
+	}
+}
+
+
+
