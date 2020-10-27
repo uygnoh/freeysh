1dd0780fb84d3c7c8509bf2fdc4558fe0e989ee9
hongyu <961832097@qq.com>
2020-10-27 04:18:31 -0400

    Tue 27 Oct 2020 04:18:31 AM EDT

--- compiler/cxx/cc/list.c
+++ compiler/cxx/cc/list.c
@@ -78,7 +78,6 @@ void print_list(struct node *head_node)
 	printf("\n");
 }
 
-
 int main(void)
 {
 	struct node *list = create_list();
@@ -91,6 +90,50 @@ int main(void)
 	print_list(list);
 	return 0;
 }
+
+
+
+
+
+
+/* 静态链表创建 */
+#include <stdio.h>
+struct student {
+	int num;
+	float score;
+	struct student *next;
+};
+int main(void)
+{
+	struct student a, b, c, *head, *p;
+	a.num = 123;	a.score = 55.78;
+	b.num = 456;	b.score = 88.78;
+	c.num = 789;	c.score = 95.78;
+
+	head = &a;
+	a.next = &b;
+	b.next = &c;
+	c.next = NULL;
+
+	p = head;
+	do {
+		printf("%d, %5.1f\n", p->num, p->score);
+		p = p->next;
+	} while (p != NULL);
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
 
 
 
