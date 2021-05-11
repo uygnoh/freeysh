446686c31aa576c33ff04540cbed46539c89bc13
uygnoh <961832097@qq.com>
2021-05-11 20:53:44 +0800

    Tue 11 May 2021 08:53:44 PM CST

--- profiles/vim/vim.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-mkdir /home/ysh/.vim/autoload
-cp /home/ysh/Documents/FREEYSH/command/utilities/vim/plug-in/vim-plug/autoload/plug.vim /home/ysh/.vim/autoload
-mkdir ~/.vim/colors/
-cp /home/ysh/Documents/FREEYSH/command/utilities/vim/plug-in/molokai/molokai.vim ~/.vim/colors/
