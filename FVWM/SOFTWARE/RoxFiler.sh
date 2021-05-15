0d71051060f64eb1e58fb8f9f19f93a36790b076
uygnoh <961832097@qq.com>
2021-05-15 10:54:25 +0800

    Sat 15 May 2021 10:54:25 AM CST

--- FVWM/SOFTWARE/RoxFiler.sh
+++ FVWM/SOFTWARE/RoxFiler.sh
@@ -1,43 +1,37 @@
 #!/bin/bash
 set -e
 
-# if [ ! -d ~/.config/rox.sourceforge.net/MIME-types ]; then
-#     mkdir ~/.config/rox.sourceforge.net/MIME-types
-# fi
-# cat > ~/.config/rox.sourceforge.net/MIME-types/application_x-shellscript <<\EOF
-#!/bin/bash
-# exec "$@"
-# EOF
 
 
-if [ ! -d ~/.config/rox.sourceforge.net/MIME-types ]; then
-    mkdir ~/.config/rox.sourceforge.net/MIME-types
+
+if [ ! -d ~/.config/rox_sourceforge_net/MIME-types ]; then
+    mkdir -p ~/.config/rox_sourceforge_net/MIME-types
 fi
-cat > ~/.config/rox.sourceforge.net/MIME-types/shellscript <<\EOF
+cat > ~/.config/rox_sourceforge_net/MIME-types/shellscript <<\EOF
 #!/bin/bash
 exec vim "$@"
 EOF
 
 
-if [ ! -d ~/.config/rox.sourceforge.net/MIME-types ]; then
-    mkdir ~/.config/rox.sourceforge.net/MIME-types
+if [ ! -d ~/.config/rox_sourceforge_net/MIME-types ]; then
+    mkdir -p ~/.config/rox_sourceforge_net/MIME-types
 fi
-cat > ~/.config/rox.sourceforge.net/MIME-types/text <<\EOF
+cat > ~/.config/rox_sourceforge_net/MIME-types/text <<\EOF
 #!/bin/bash
 exec gedit "$@"
 EOF
 
 
-if [ ! -d ~/.config/rox.sourceforge.net/MIME-types ]; then
-    mkdir ~/.config/rox.sourceforge.net/MIME-types
+if [ ! -d ~/.config/rox_sourceforge_net/MIME-types ]; then
+    mkdir -p ~/.config/rox_sourceforge_net/MIME-types
 fi
-cat > ~/.config/rox.sourceforge.net/MIME-types/smplayer <<\EOF
+cat > ~/.config/rox_sourceforge_net/MIME-types/smplayer <<\EOF
 #!/bin/bash
 exec smplayer "$@"
 EOF
 
 
-cd ~/.config/rox.sourceforge.net/MIME-types
+cd ~/.config/rox_sourceforge_net/MIME-types
 chmod 755 *
 
 
