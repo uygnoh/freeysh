14cb98b95fd3f387480ef279b541289e1951a9b4
uygnoh <961832097@qq.com>
2021-05-15 09:52:57 +0800

    Sat 15 May 2021 09:52:57 AM CST

---
+++ FVWM/SOFTWARE/RoxFiler.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e
+
+if [ ! -d ~/.config/rox.sourceforge.net/MIME-types ]; then
+    mkdir ~/.config/rox.sourceforge.net/MIME-types
+fi
+cat > ~/.config/rox.sourceforge.net/MIME-types/application_x-shellscript <<\EOF
+#!/bin/bash
+exec "$@"
+EOF
+
+
+if [ ! -d ~/.config/rox.sourceforge.net/MIME-types ]; then
+    mkdir ~/.config/rox.sourceforge.net/MIME-types
+fi
+cat > ~/.config/rox.sourceforge.net/MIME-types/text <<\EOF
+#!/bin/bash
+gedit "$@"
+EOF
+
+
+if [ ! -d ~/.config/rox.sourceforge.net/MIME-types ]; then
+    mkdir ~/.config/rox.sourceforge.net/MIME-types
+fi
+cat > ~/.config/rox.sourceforge.net/MIME-types/smplayer <<\EOF
+#!/bin/bash
+smplayer "$@"
+EOF
+
+
+cd ~/.config/rox.sourceforge.net/MIME-types
+chmod 755 *
+
+
