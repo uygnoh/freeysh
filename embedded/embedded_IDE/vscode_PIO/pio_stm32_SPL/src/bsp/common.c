c21aa82f688609f0c7c0e828e8ebbaddbfb2894c
uygnoh <961832097@qq.com>
2021-05-28 13:56:12 +0800

    Fri 28 May 2021 01:56:12 PM CST

--- embedded/embedded_IDE/vscode_PIO/pio_stm32_SPL/src/bsp/common.c
+++ embedded/embedded_IDE/vscode_PIO/pio_stm32_SPL/src/bsp/common.c
@@ -5,7 +5,8 @@ void delay_us(u32 nus)
 	 u32 temp;
 	 SysTick->LOAD = 9*nus; 
 	 SysTick->VAL  = 0X00; 
-	 SysTick->CTRL = 0X01; 
+	 SysTick->CTRL = 0X01;
+	 
 	 do
 	 {
 		temp = SysTick->CTRL; 
@@ -22,7 +23,8 @@ void delay_ms(u32 nms)
 	 u32 temp;
 	 SysTick->LOAD = 9000*nms;
 	 SysTick->VAL  = 0X00; 
-	 SysTick->CTRL = 0X01; 
+	 SysTick->CTRL = 0X01;
+	 
 	 do
 	 {
 		temp = SysTick->CTRL; 
